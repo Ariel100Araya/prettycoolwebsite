ac8f4cf61c2e82baa2fa42410b189d70fc65a563
Ariel Araya-Madrigal <58918901+Ariel100Araya@users.noreply.github.com>
2026-05-01 15:06:15 -0400

    App Clip Support

---
+++ interesting way of doing experience.js
@@ -0,0 +1,50 @@
+let name = "Ariel Araya"
+let skills = ['Swift', 'SwiftUI', 'Python', 'JavaScript', 'Java', 'Node.js', 'CSS', 'HTML']
+let experience = [
+    {
+        role: "iOS Software Engineer",
+        duration: "2020 - Present",
+        responsibilities: [
+            "Developed and maintained iOS applications using Swift and SwiftUI.",
+            "Collaborated with cross-functional teams to design and implement new features.",
+            "Optimized application performance and fixed bugs to enhance user experience."
+        ],
+        honors: [
+            "Swift Student Challenge Winner - 2022",
+            "Certificate in Computer Science - Frederick Community College - 2026",
+            "Self-taught iOS Developer with over 5 years of experience.",
+            "Contributed to open-source iOS projects and published apps on the App Store."
+        ]
+    }, 
+    {
+        role: "Cybersecurity Analyst",
+        duration: "2022 - Present",
+        responsibilities: [
+            "Conducted security assessments and vulnerability testing to identify potential threats.",
+            "Implemented security measures and protocols to protect sensitive data.",
+            "Monitored network traffic and responded to security incidents in a timely manner."
+        ],
+        honors: [
+            "COMPTIA A+ Certified - 2024",
+            "COMPTIA Security+ trained - 2025",
+            "ISC2 CC Certified - 2025",
+            "Associate of Cybersecurity - Frederick Community College - 2026",
+            "In Progress - Bachelor of Cybersecurity - University of Maryland, Global Campus - 2028"
+        ]
+    },
+    {
+        role: "IT Specialist",
+        duration: "2020 - Present",
+        responsibilities: [
+            "Conducted security assessments and vulnerability testing to identify potential threats.",
+            "Implemented security measures and protocols to protect sensitive data.",
+            "Monitored network traffic and responded to security incidents in a timely manner."
+        ],
+        honors: [
+            "COMPTIA A+ Certified - 2024",
+            "COMPTIA Security+ trained - 2025",
+            "ISC2 CC Certified - 2025",
+            "Associate of Cybersecurity - Frederick Community College - 2026",
+            "In Progress - Bachelor of Cybersecurity - University of Maryland, Global Campus - 2028"
+        ]
+    },
